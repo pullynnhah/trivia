bf0e35300a021f0aff8d193c35ea01a13e823fee
Paula Rodrigues <paulinhaabro@gmail.com>
2022-06-14 17:52:30 -0300

    implement function to display quizes and the style for then

--- scripts/script.js
+++ scripts/script.js
@@ -7,12 +7,11 @@ function renderFormAmountHTML(start, end) {
       options += `<option>${i}</option>`;
     }
   }
-  console.log(options);
   amount.innerHTML = options;
 }
 
 function renderFormCategoryHTML() {
-  const promise = axios.get("https://opentdb.com/api_category.php");
+  const promise = axios.get(`${OPEN_TDB_URL}/api_category.php`);
   promise.then(response => {
     const categories = response.data["trivia_categories"];
     let options = '<option value="" selected="selected">Any Category</option>';
@@ -25,27 +24,96 @@ function renderFormCategoryHTML() {
 
 function submitForm(event) {
   event.preventDefault();
-  // TODO: inplement object from form results
+  getParams();
+  generateQuiz();
 }
 
+function getParams() {
+  const amountValue = amount.value;
+  const categoryValue = category.value;
+  const difficultyValue = difficulty.value;
+  const typeValue = type.value;
+
+  params = {};
+  params.amount = amountValue;
+
+  if (categoryValue !== "") {
+    params.category = categoryValue;
+  }
+
+  if (difficultyValue !== "") {
+    params.difficulty = difficultyValue;
+  }
+
+  if (typeValue !== "") {
+    params.type = typeValue;
+  }
+}
+
+function generateQuiz() {
+  const promise = axios.get(`${OPEN_TDB_URL}/api.php`, {params: params});
+  promise.then(response => {
+    quizes = response.data.results;
+    sizeQuizes = quizes.length;
+    loadQuizes();
+  });
+}
 
 function loadForm() {
-  if (form.classList.contains('hidden')) {
-    form.classList.remove('hidden');
+  if (form.classList.contains("hidden")) {
+    form.classList.remove("hidden");
   }
+  
+  section.classList.add('hidden');
   renderFormAmountHTML(1, 50);
   renderFormCategoryHTML();
 }
 
+function loadQuizes() {
+  form.classList.add('hidden');
+  if (section.classList.contains('hidden')) {
+    section.classList.remove('hidden');
+  }
 
-const form = document.querySelector('form');
+  let quizesHTML = '';
+  for (const quiz of quizes) {
+    quizesHTML += quizHTML(quiz);
+  }
+  
+  section.innerHTML = quizesHTML;
+  section.innerHTML += '<button onclick="getResults()">Get Results</button>'
+}
+
+function quizHTML(quiz) {
+  const options = [];
+  options.push(`<p class="correct">${quiz.correct_answer}</p>`);
+  for (const incorrectAnswer of quiz.incorrect_answers) {
+    options.push(`<p>${incorrectAnswer}</p>`)
+  }
+  
+  options.sort(() => Math.random() - 0.5);
+  console.log(options.join('\n'));
+  return `
+    <article>
+      <h3>${quiz.question}</h3>
+      <div>${options.join('')}</div>
+    </article>`
+}
+
+// function loadGame
+const OPEN_TDB_URL = "https://opentdb.com";
+
+const form = document.querySelector("form");
 const amount = document.querySelector("#amount");
 const category = document.querySelector("#category");
 const difficulty = document.querySelector("#difficulty");
 const type = document.querySelector("#type");
 const submitBtn = document.querySelector("button");
+const section = document.querySelector('section');
 
-
+let params = null;
+let quizes = null;
+let sizeQuizes = null;
 submitBtn.addEventListener("click", submitForm);
 
 loadForm();
