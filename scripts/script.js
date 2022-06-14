39719f59c54603c9c2d08ac7e0b2f3c923248d13
Paula Rodrigues <paulinhaabro@gmail.com>
2022-06-14 15:05:45 -0300

    change font add button starter click event

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+function submitForm(event) {
+  event.preventDefault();
+  // TODO: inplement object from form results
+}
+
+const submitBtn = document.querySelector("button");
+submitBtn.addEventListener("click", submitForm);
